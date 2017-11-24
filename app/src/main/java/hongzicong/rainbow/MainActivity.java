9c4f2bc43bc11e7947b525b345172b9a7bda2321
hongzicong <973138379@qq.com>
2017-11-25 00:32:04 +0800

    2.初步建造tab、沉浸式还有会改变的toolbar

--- app/src/main/java/hongzicong/rainbow/MainActivity.java
+++ app/src/main/java/hongzicong/rainbow/MainActivity.java
@@ -1,36 +1,51 @@
 package hongzicong.rainbow;
 
+import android.os.Build;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private NavigationView navigationView;
     private Toolbar toolBar;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
         initAllWidget();
         setAllListener();
         setMyActionBar();
+        Log.d("MainActivityHaHa",getStatusBarHeight()+" ");
     }
 
     private void initAllWidget(){
-        mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        //沉浸式设置
+        tabLayout=(TabLayout)findViewById(R.id.tablayout);
         toolBar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolBar);
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
         navigationView=(NavigationView)findViewById(R.id.nav_view);
         //默认选哪个值，记得修改
         navigationView.setCheckedItem(R.id.nav_main);
+        tabLayout.addTab(tabLayout.newTab().setText("专家"));
+        tabLayout.addTab(tabLayout.newTab().setText("我"));
     }
 
     private void setAllListener(){
@@ -60,5 +75,14 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 }
