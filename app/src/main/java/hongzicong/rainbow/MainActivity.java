707de928f6cb47147cbf5c1836d51b41519aa819
hongzicong <973138379@qq.com>
2017-11-26 20:11:17 +0800

    8.玛德垃圾bug，终于弄好了，最好不要将toolbar放进去一个layout中，然后再将layout放进appbarlayout中

--- app/src/main/java/hongzicong/rainbow/MainActivity.java
+++ app/src/main/java/hongzicong/rainbow/MainActivity.java
@@ -32,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+                //将侧边栏顶部延伸至status bar
+                mDrawerLayout.setFitsSystemWindows(true);
+                //将主页面顶部延伸至status bar;虽默认为false,但经测试,DrawerLayout需显示设置
+                mDrawerLayout.setClipToPadding(false);
+            }
+        }
         initAllWidget();
         setAllListener();
         setMyActionBar();
@@ -46,17 +56,6 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
         navigationView=(NavigationView)findViewById(R.id.nav_view);
         bottomNavigationBar=(BottomNavigationBar)findViewById(R.id.bottom_navigation_bar);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
-            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
-            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
-                //将侧边栏顶部延伸至status bar
-                mDrawerLayout.setFitsSystemWindows(true);
-                //将主页面顶部延伸至status bar;虽默认为false,但经测试,DrawerLayout需显示设置
-                mDrawerLayout.setClipToPadding(false);
-            }
-        }
 
         //Test
         textView=(TextView)findViewById(R.id.test_textView);
